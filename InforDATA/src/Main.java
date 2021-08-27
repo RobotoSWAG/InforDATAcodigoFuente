93565d0d251d6944960ddd1a7fe6556d1986ddab
RobotoSWAG <74079951+RobotoSWAG@users.noreply.github.com>
2021-08-27 11:02:25 -0400

    Add files via upload

--- InforDATA/src/Main.java
+++ InforDATA/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Impresoras impresoras = new Impresoras(1234, "elnuevo", "si", 01-11-21);
+       /* Impresoras impresoras = new Impresoras(1234, "elnuevo", "si", 01-11-21);
         Provedores provedores = new Provedores("2037dfh28e", "sucasa",10-07-21, 2 );
         OtrosProductos otrosProductos = new OtrosProductos(19234856, "bruh", 01-12-21);
         CPU cpu = new CPU(98723, "wowomg", "1tb", 01-01-21, "wakanda", new Empreza("empreza", "africahaha", 420, 69, true, new Cliente("380u2f", 65515929, "sucasatambien", 18-11-20)));
@@ -8,7 +8,12 @@ public class Main {
         Cliente cliente = new Cliente("380u2f", 65515929, "sucasatambien", 18-11-20);
         Monitores monitores = new Monitores(189723, "Moon67", "1000hz", 12-12-12, "antarctica", new Empreza("empreza", "africahaha", 420, 69, true, new Cliente("380u2f", 65515929, "sucasatambien", 18-11-20)));
         Alquilar alquilar = new Alquilar(71234, 1232, new Monitores(189723, "Moon67", "1000hz", 12-12-12, "antarctica", new Empreza("empreza", "africahaha", 420, 69, true, new Cliente("380u2f", 65515929, "sucasatambien", 18-11-20))), new CPU(98723, "wowomg", "1tb", 01-01-21, "wakanda", new Empreza("empreza", "africahaha", 420, 69, true, new Cliente("380u2f", 65515929, "sucasatambien", 18-11-20))));
-        
+
+*/
+        OtrosProductos w200 = new Impresoras(10, "qwoij", "rapido", 010101);
+        System.out.println(w200);
+
+        Impresoras impresoras = new Impresoras(w200);
 
     }
 }
